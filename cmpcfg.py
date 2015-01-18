4043c776133cca7b012b384811b5180ab23274c8
J.M. Dana <jmdana@gmail.com>
2015-01-18 19:49:19 +0000

    Show line numbers and sort the result

--- cmpcfg.py
+++ cmpcfg.py
@@ -4,11 +4,39 @@
 import sys
 import re
 import argparse
+from operator import itemgetter
 
 COMMENT_CHAR = '#'
 
 def print_separator():
     print("=" * 70)
+
+def simple_diff(set1, set2, fmt):
+    result = [(set1[line], line) for line in set(set1.keys()) - set(set2.keys())]
+
+    for idx, line in sorted(result, key=itemgetter(0)):
+        print(fmt.format(idx=idx, line=line))
+
+def diff(set1, set2, numbering=True):
+    if numbering:
+        import math
+        max_val = max([x for x in list(set1.values()) + list(set2.values())])
+        digits = int(math.log10(max_val))+1
+        fmt_idx = " {idx:0%dd}: {line:s}" % digits
+    else:
+        fmt_idx = " {line:s}"
+
+    print_separator()
+
+    fmt = "<" + fmt_idx
+    simple_diff(set1, set2, fmt)
+
+    print_separator()
+
+    fmt = ">" + fmt_idx
+    simple_diff(set2, set1, fmt)
+
+    print_separator()
 
 def scan_file(fname, comment):
     """Scans the configuration file ignoring comments.
@@ -21,20 +49,21 @@ def scan_file(fname, comment):
 
     f = open(fname, 'r')
 
-    contents = []
+    contents = {}
     comment_re = re.compile(r"^\s*%s" % comment)
 
-    for line in f.readlines():
+    for idx, line in enumerate(f.readlines()):
         if comment_re.match(line):
             continue
 
         line = line.strip()
 
         if line:
-            contents.append(line)
+            # We don't care about duplicates, just keep one
+            contents[line] = idx + 1
 
     f.close()
-    return set(contents)
+    return contents
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -47,6 +76,20 @@ if __name__ == '__main__':
         action='store',
         help='the character preceding a comment (default: %s)'
         % COMMENT_CHAR)
+
+    parser.add_argument(
+        '--numbers',
+        dest='numbers',
+        action='store_true',
+        help="show numbering (default: true)")
+
+    parser.add_argument(
+        '--no-numbers',
+        dest='numbers',
+        action='store_false',
+        help="don't show numbering (default: false)")
+
+    parser.set_defaults(numbers=True)
 
     parser.add_argument('file1', type=str, default=None, help='The first file')
     parser.add_argument('file2', type=str, default=None, help='The second file')
@@ -69,16 +112,6 @@ if __name__ == '__main__':
         print("They are equivalent")
         sys.exit(0)
 
-    print_separator()
-
-    for line in contents1 - contents2:
-        print("< %s" % line)
-
-    print_separator()
-
-    for line in contents2 - contents1:
-        print("> %s" % line)
-
-    print_separator()
+    diff(contents1, contents2, args.numbers)
 
 
